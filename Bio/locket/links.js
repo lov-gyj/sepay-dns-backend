34885e6673d12c3471577f0fbfc8a8fa93587424
lov-gyj <bbi.younj@gmail.com>
2026-07-30 21:43:53 +0700

    Delete Bio directory

--- Bio/locket/links.js
+++
@@ -1,5 +0,0 @@
-// File cấu hình link tải ứng dụng
-const DOWNLOAD_LINKS = {
-    direct: "#",        // Link tải trực tiếp (Thay bằng link của bạn hoặc để "#" nếu chưa có)
-    ipa: "https://example.com/locket.ipa" // Link file IPA (Thay bằng link của bạn hoặc để "#" nếu chưa có)
-};
